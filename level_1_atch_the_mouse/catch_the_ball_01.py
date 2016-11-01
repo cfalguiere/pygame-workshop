e0fc28c085372f8a256bfb87b75664ec5345c226
Claude Falguiere <cfalguiere@gmail.com>
2016-11-01 22:17:29 +0100

    renommage et ajout image souris

--- level_1_atch_the_mouse/catch_the_ball_01.py
+++ level_1_atch_the_mouse/catch_the_ball_01.py
@@ -2,13 +2,13 @@
 # Détecter la position de la souris et déplacer la balle avec la souris
 #
 # initialise pygame
-import sys, pygame
+import pygame
 pygame.init()
 
 # défini le canvas d'affichage et les couleurs
 window_size = width, height = 320, 240
-i_hor = 0
-i_ver = 1
+X = 0
+Y = 1
 color_black = ( 0, 0, 0 )
 screen = pygame.display.set_mode(window_size)
 
@@ -17,10 +17,11 @@ ball = pygame.image.load("ball.bmp")
 ballrect = ball.get_rect()
 
 # boucle infinie
-while True:
+running = True
+while running:
     for event in pygame.event.get():
         # pour le bouton de fermeture de la fenêtre, quitte le programme
-        if event.type == pygame.QUIT: sys.exit()
+        if event.type == pygame.QUIT: running = False
         # pour la souris
         if event.type == pygame.MOUSEMOTION:
           ballrect.center = pygame.mouse.get_pos()
---
+++ level_1_atch_the_mouse/catch_the_mouse.py
@@ -0,0 +1,55 @@
+#
+# Détecter la position de la souris et déplacer la balle avec la souris
+# Si on clique sur la souris, elle fuit ailleurs
+#
+
+# initialise pygame
+import random, pygame
+pygame.init()
+
+# défini la zone d'affichage et les couleurs
+window_size = width, height = 320, 240
+X = 0
+Y = 1
+color_white = ( 255, 255, 255 )
+game_surface = pygame.display.set_mode(window_size)
+
+# charge l'image de la balle
+mouse_image = pygame.image.load("mouse_mini.png")
+mouse_rect = mouse_image.get_rect()
+
+# boucle infinie
+running = True
+grab = False
+mouse_image_pos = [ width/2, height/2 ]
+while running:
+    for event in pygame.event.get():
+        print (event)
+        mouse_pos = pygame.mouse.get_pos()
+        # pour le bouton de fermeture de la fenêtre, quitte le programme
+        if event.type == pygame.QUIT: running = False
+        # pour la souris
+        if event.type == pygame.MOUSEMOTION:
+           if grab:
+              mouse_image_pos = mouse_pos
+        if event.type == pygame.MOUSEBUTTONDOWN:
+           if mouse_rect.collidepoint(mouse_pos):
+              x = ( mouse_pos[X] + random.randint(80,250) ) % width
+              y = ( mouse_pos[Y] + random.randint(60,200) ) % height
+              mouse_image_pos = [ x, y ]
+              grab = False
+           else :
+              grab = True
+        if event.type == pygame.MOUSEBUTTONUP:
+           if grab:
+              mouse_image_pos = mouse_pos
+           grab = False
+
+    # repositionne l'image
+    mouse_rect.center = mouse_image_pos
+
+    # reaffiche le fond et la balle
+    game_surface.fill(color_white)
+    game_surface.blit(mouse_image, mouse_rect)
+    pygame.display.flip()
+
